86a1be9502015be68aef2938a3b6445a5df67150
TopFps <TopFps@DESKTOP-UUEU6H0>
2022-10-31 21:10:41 +0300

    Добавьте файлы проекта.

---
+++ Задание_6_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strStart = Console.ReadLine();
+            string[] strArr = strStart.Split(); 
+            string maxStr = strArr[0];
+            foreach (string str in strArr)
+            {
+                if (str.Length > maxStr.Length)
+                { 
+                    maxStr = str;   
+                }
+            }
+            Console.WriteLine(maxStr);  
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ Задание_6_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string strStart = str.ToLower().Replace(" ", "");
+            int i = 0;
+            int flag = strStart.Length;
+
+            for (i = 0; (i < (strStart.Length - 1)); i++)
+            {
+                if (strStart[i] != strStart[strStart.Length - 1 - i])
+                {
+                    Console.WriteLine("Строка не является палиндромом");
+                    break;
+                }
+                else
+                {
+                    flag -- ;
+                }
+            }
+            if (flag == 1)
+            { Console.WriteLine("Строка является палиндромом"); }
+            Console.ReadKey();
+        }
+    }
+}
